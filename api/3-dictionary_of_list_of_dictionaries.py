f192fde46f85018156fd076d8ef8e38520fcb5a3
Amelieumutoni <a.umutoni@alustudent.com>
2024-02-24 00:19:22 -0800

    3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
-''' Test request to parse API's
-'''
+"""Script that gets user data (Todo list) from API
+and then export the result to csv file. """
 
-
-import csv
 import json
 import requests
-import sys
 
-if __name__ == "__main__":
-    api_endpoint = "https://jsonplaceholder.typicode.com"
 
-    def get_user_tasks(id):
-        '''Return data for the user id passed as an argument
-        '''
-        user_id = str(id)
-        user_data = requests.get(api_endpoint + "/users/" + user_id).json()
-        username = user_data.get('username')
-        todo_data = \
-            requests.get(api_endpoint + "/users/" + user_id + "/todos").\
-            json()
-        tasks = []
-        for task in todo_data:
-            tasks.append({'username': username,
-                          'task': task['title'],
-                          'completed': task['completed']})
-        data = {"{}".format(user_id): tasks}
-        return data
+def main():
+    """main function"""
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
 
-    all_users = requests.get(api_endpoint + "/users").json()
-    all_json = {}
-    for user in all_users:
-        user_data = get_user_tasks(user.get('id'))
-        all_json.update(user_data)
-    with open("todo_all_employees.json", 'w') as data_file:
-        json.dump(all_json, data_file)
+    response = requests.get(todo_url)
+
+    output = {}
+
+    for todo in response.json():
+        user_id = todo.get('userId')
+        if user_id not in output.keys():
+            output[user_id] = []
+            user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
+                user_id)
+            user_name = requests.get(user_url).json().get('username')
+
+        output[user_id].append(
+            {
+                "username": user_name,
+                "task": todo.get('title'),
+                "completed": todo.get('completed')
+            })
+
+    with open("todo_all_employees.json", 'w') as file:
+        json.dump(output, file)
+
+
+if __name__ == '__main__':
+    main()
