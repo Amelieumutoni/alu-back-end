c6ff3746cae2e682b64e89fac89dc8d35f34d7e5
Amelieumutoni <a.umutoni@alustudent.com>
2024-02-24 00:24:04 -0800

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,10 +6,6 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
-        sys.exit(1)
-
     employee_Id = int(sys.argv[1])
 
     todo_url = "https://jsonplaceholder.typicode.com/todos"
@@ -17,45 +13,23 @@ if __name__ == "__main__":
 
     user_response = requests.get(user_data_url)
     todo_response = requests.get(todo_url)
-
-    if user_response.status_code != 200 or todo_response.status_code != 200:
-        print("Failed to fetch data from API.")
-        sys.exit(1)
-
+    # if todo_response.status_code & user_response.status_code == 200:
     todos = todo_response.json()
     users = user_response.json()
-
-    employee_name = None
-
     for user in users:
         if user.get("id") == employee_Id:
             employee_name = user.get("name")
-            break
-
-    if employee_name is None:
-        print("Employee ID not found.")
-        sys.exit(1)
-
     # filter completed tasks
     done = []
     total = 0
     completed = 0
-
     for todo in todos:
         if todo.get("userId") == employee_Id:
             total += 1
             if todo.get("completed"):
                 completed += 1
                 done.append(todo.get("title"))
-
     # Display the progress information
-    print(f"Employee Name: {employee_name}")
-    print(f"Tasks Completed: {completed}/{total}")
-
-    if completed > 0:
-        print("Completed Tasks:")
-        for idx, task in enumerate(done, 1):
-            print(f"Task {idx}: {task}")
-    else:
-        print("No tasks completed.")
-
+    print(f"Employee {employee_name} is done with tasks({completed}/{total}):")
+    for _ in done:
+        print(f"\t {_}")
